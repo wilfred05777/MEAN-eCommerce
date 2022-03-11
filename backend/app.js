b6964df548ae80cb82c9b292164e5b82ab862edf
Wilfred <wilfred5777@gmail.com>
2022-03-11 19:02:22 +0800

    14. Create First API Call & Parsing Json Data

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,23 @@ require("dotenv/config");
 
 const api = process.env.API_URL;
 
+// Middleware
+app.use(express.json());
+
 //// http://localhost:3000/api/v1/products
-app.get(api + "/products", (req, res) => {
-  res.send("Hello Api!");
+app.get(`${api}/products`, (req, res) => {
+  const product = {
+    id: 1,
+    name: "Hair Dresser",
+    image: "some_url",
+  };
+  res.send(product);
+});
+
+app.post(`${api}/products`, (req, res) => {
+  const newProduct = req.body;
+  console.log(newProduct);
+  res.send(product);
 });
 
 app.listen(3000, () => {
