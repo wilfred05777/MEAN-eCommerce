1bc7ec6115f895ee9bad86bd3135a648cfec0188
Wilfred <wilfred5777@gmail.com>
2022-03-11 18:44:55 +0800

    12. Creating the Backend Server with Express

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,10 @@
-console.log("hello World");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Api!");
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running http://localhost:3000`);
+});
