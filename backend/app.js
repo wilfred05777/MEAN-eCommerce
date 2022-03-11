951a93c40c5e7667a5b7171ccbdf6d4d61fcc037
Wilfred <wilfred5777@gmail.com>
2022-03-11 18:55:43 +0800

    13. Reading Environment Variables

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
+require("dotenv/config");
+
+const api = process.env.API_URL;
+
+//// http://localhost:3000/api/v1/products
+app.get(api + "/products", (req, res) => {
   res.send("Hello Api!");
 });
 
 app.listen(3000, () => {
+  console.log(api);
   console.log(`Server is running http://localhost:3000`);
 });
