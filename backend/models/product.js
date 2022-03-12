9ace8c706406eb80cdf09391ac900f48e31eedc9
Wilfred <wilfred5777@gmail.com>
2022-03-13 06:12:10 +0800

    26. Products Model & Schema

--- backend/models/product.js
+++ backend/models/product.js
@@ -2,11 +2,60 @@ const mongoose = require("mongoose");
 
 //// Example of Product schema
 const productSchema = mongoose.Schema({
-  name: String,
-  image: String,
-  countInStock: {
-    require: true,
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  richDescription: {
+    type: String,
+    default: "",
+  },
+  image: {
+    type: String,
+    default: "",
+  },
+  images: [
+    {
+      type: String,
+    },
+  ],
+  brand: {
+    type: String,
+    default: "",
+  },
+  price: {
     type: Number,
+    default: 0,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 255,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  numReviews: {
+    type: Number,
+    default: 0,
+  },
+  isFeatured: {
+    type: Boolean,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
   },
 });
 
