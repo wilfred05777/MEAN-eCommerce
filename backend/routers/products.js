3291745134fb062d77bb5476a786d32c17e92e9e
Wilfred <wilfred5777@gmail.com>
2022-03-16 05:26:05 +0800

    36. Get Products Count for Statistics Purposes -NW

--- backend/routers/products.js
+++ backend/routers/products.js
@@ -143,4 +143,15 @@ router.delete("/:id", (req, res) => {
     });
 });
 
+router.get(`/get/count`, async (req, res) => {
+  const productCount = await Product.countDocuments((count) => count);
+
+  if (!productCount) {
+    res.status(500).json({ success: false });
+  }
+  res.send({
+    productCount: productCount,
+  });
+});
+
 module.exports = router;
