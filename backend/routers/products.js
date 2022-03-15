5ed78167fb7c36daca8462d0e56fec90c18dc4d4
Wilfred <wilfred5777@gmail.com>
2022-03-15 10:32:21 +0800

    32. Get a Product & List of Products REST API

--- backend/routers/products.js
+++ backend/routers/products.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get(`/`, async (req, res) => {
-  const productList = await Product.find();
+  const productList = await Product.find().select("name image -_id");
 
   if (!productList) {
     res.status(500).json({ success: false });
@@ -18,25 +18,50 @@ router.get(`/`, async (req, res) => {
   // res.send(product);
 });
 
-router.post(`/`, (req, res) => {
+router.get(`/:id`, async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    res.status(500).json({ success: false });
+  }
+  res.send(product);
+});
+
+router.post(`/`, async (req, res) => {
+  const category = await Category.findById(req.body.category);
+
+  if (!category) return res.status(400).send("Invalid Category");
+
   const product = new Product({
     name: req.body.name,
     image: req.body.image,
+    description: req.body.description,
+    richDescription: req.body.richDescription,
+    image: req.body.image,
+    brand: req.body.brand,
+    price: req.body.price,
+    category: req.body.category,
     countInStock: req.body.countInStock,
+    rating: req.body.rating,
+    numReviews: req.body.numReviews,
+    isFeatured: req.body.isFeatured,
   });
+  product = await product.save();
 
-  product
-    .save()
-    .then((createdProduct) => {
-      res.status(201).json(createdProduct);
-    })
-    .catch((error) => {
-      res.status(500).json({
-        error: err,
-        success: false,
-      });
-    });
+  if (!product) return res.status(500).send("The product cannot be created");
 
+  res.send(product);
+  // product
+  //   .save()
+  //   .then((createdProduct) => {
+  //     res.status(201).json(createdProduct);
+  //   })
+  //   .catch((error) => {
+  //     res.status(500).json({
+  //       error: err,
+  //       success: false,
+  //     });
+  //   });
   // const newProduct = req.body;
   // console.log(newProduct);
   // res.send(newProduct);
