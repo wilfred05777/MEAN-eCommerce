28cf8e659a0a08f5dc42c38f6490c7b457a0aa0e
Wilfred <wilfred5777@gmail.com>
2022-03-15 10:37:34 +0800

    33. Show Category Details in the Product - Populate

--- backend/routers/products.js
+++ backend/routers/products.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.get(`/`, async (req, res) => {
   //// Learning on -exclude and show only specifice fields like: name image
   const productList = await Product.find().select("name image -_id");
+  // const productList = await Product.find().populate("category");
 
   if (!productList) {
     res.status(500).json({ success: false });
@@ -20,7 +21,7 @@ router.get(`/`, async (req, res) => {
 });
 
 router.get(`/:id`, async (req, res) => {
-  const product = await Product.findById(req.params.id);
+  const product = await Product.findById(req.params.id).populate("category");
 
   if (!product) {
     res.status(500).json({ success: false });
