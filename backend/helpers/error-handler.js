5e9093f995965d4f601fe5b6f5e82c5df2d378e8
Wilfred <wilfred5777@gmail.com>
2022-03-16 10:54:34 +0800

    49. Authentication Error Handling

--- backend/helpers/error-handler.js
+++ backend/helpers/error-handler.js
@@ -1,20 +1,19 @@
 function errorHandler(err, req, res, next) {
-  //   if (err) {
-  //     res.status(500).json({ message: err });
-  //   }
-
   if (err.name === "UnauthorizedError") {
     // jwt authentication error
     return res.status(401).json({ message: "The user is not authorized" });
   }
-
   if (err.name === "ValidationError") {
     //  validation error
     return res.status(401).json({ message: err });
   }
 
-  // default to 500 server error
+  // GENERAL ERROR or default to 500 server error
   return res.status(500).json(err);
+
+  //   if (err) {
+  //     res.status(500).json({ message: err });
+  //   }
 }
 
 module.exports = errorHandler;
