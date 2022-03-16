7b95ab946807e44afa920bed29fe7f91cd0e6971
Wilfred <wilfred5777@gmail.com>
2022-03-16 09:35:50 +0800

    46. Update User Data With/Without Password ERROR

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -62,4 +62,39 @@ router.post(`/`, async (req, res) => {
   //   });
 });
 
+// @desc Update User
+// @route PUT /api/v1/users/:id
+// @access private
+router.put("/:id", async (req, res) => {
+  const userExist = await Users.findById(req.params.id);
+  let newPassword;
+  if (req.body.password) {
+    newPassword = bcrypt.hashSync(req.body.password, 10);
+  } else {
+    newPassword = userExist.passwordHash;
+  }
+
+  const user = await Users.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+      passwordHash: bcrypt.hashSync(req.body.password, 10),
+      // passwordHash: req.body.passwordHash,
+      phone: req.body.phone,
+      isAdmin: req.body.isAdmin,
+      street: req.body.street,
+      apartment: req.body.apartment,
+      zip: req.body.zip,
+      city: req.body.city,
+      country: req.body.country,
+    },
+    { new: true }
+  );
+
+  if (!user) return res.status(400).send("the user cannot be created!");
+
+  res.send(user);
+});
+
 module.exports = router;
