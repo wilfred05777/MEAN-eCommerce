35b8b6b7223f6ed9cb99db78b5e888a64e717242
Wilfred <wilfred5777@gmail.com>
2022-03-22 05:52:30 +0800

    69. Validating Uploaded File Types

--- backend/routers/products.js
+++ backend/routers/products.js
@@ -6,13 +6,27 @@ const mongoose = require("mongoose");
 const multer = require("multer");
 // const asyncErrorHandler = require("async-error-handler");
 
+const FILE_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpeg",
+  "image/jpg": "jpg",
+};
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
+    const isValid = FILE_TYPE_MAP[file.mimetype];
+    let uploadError = new Error("Invalid Image type");
+
+    if (isValid) {
+      uploadError = null;
+    }
     cb(null, "public/uploads");
   },
   filename: function (req, file, cb) {
     // const fileName = file.originalname.replace(" ", "-");
+    // file.mimetype;
     const fileName = file.originalname.split(" ").join("-");
+    const extension = FILE_TYPE_MAP[file.mimetype];
     cb(null, `${fileName}-${Date.now()}.${extension}`);
   },
 });
