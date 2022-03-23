fe3aedfc548b323ff490cef54adef13626126c40
Wilfred <wilfred5777@gmail.com>
2022-03-23 17:42:11 +0800

    admin pages/dashboard

--- myCompany/bluebits/apps/admin/src/app/app.module.ts
+++ myCompany/bluebits/apps/admin/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, ErrorPageComponent],
+  declarations: [AppComponent, NxWelcomeComponent, ErrorPageComponent, DashboardComponent],
   imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' })],
   providers: [],
   bootstrap: [AppComponent]
