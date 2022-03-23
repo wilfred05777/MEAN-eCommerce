8907d2515edbc94c50c3c93e62de4854586adac2
Wilfred <wilfred5777@gmail.com>
2022-03-23 17:34:06 +0800

    89. NX ESLint: Enabling Live Coding Linting

--- myCompany/bluebits/apps/admin/src/app/app.module.ts
+++ myCompany/bluebits/apps/admin/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, ErrorPageComponent],
   imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' })],
   providers: [],
   bootstrap: [AppComponent]
